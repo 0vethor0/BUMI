b3da6d1f038226cfd0c441001b02f1bba3e56240
0vethor0 <161180971+0vethor0@users.noreply.github.com>
2025-07-01 13:49:31 -0400

    fix: solucion de problemas React-Router-Dom

---
+++ client/src/pages/layout/moduloTutores.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import styles from '../styles/ModuloTutores.module.css';
+
+const ModuloTutores = () => {
+    return (
+        <>
+            Introduce tu codigo aqui, pero elimina lo siguiente: "<> </>"
+        </>
+    )
+};
+
+export default ModuloTutores;
